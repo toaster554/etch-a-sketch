aaed175730895cd00e3c5fd320d90d0c0d2f5a80
Nathan Nguyen <nguyenbut24@gmail.com>
2021-05-09 17:40:53 -0400

    Added coloring scheme

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function colorCell(cell, color) {
-    console.log(cell);
-    cell.style.backgroundColor = color;
+let curColor = 'black';
+
+function colorCell() {
+    if (curColor === 'rainbow') {
+        this.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    } else {
+        this.style.backgroundColor = curColor;
+    }
 }
 
 function createCanvas(size) {
@@ -11,7 +16,7 @@ function createCanvas(size) {
     for (let i = 0; i < size**2; i++) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
-        cell.addEventListener('mouseover', (e) => colorCell(e.target, 'black'));
+        cell.addEventListener('mouseover', colorCell);
         canvas.appendChild(cell);
     }
 
@@ -19,4 +24,32 @@ function createCanvas(size) {
     canvasContainer.appendChild(canvas);
 }
 
-createCanvas(8);
+function resetCanvas() {
+    let cells = document.querySelectorAll('.canvas > .cell');
+    cells.forEach(cell => cell.style.backgroundColor = '');
+}
+
+function resizeCanvas() {
+    let newSize = parseInt(document.querySelector('#canvas-size').value);
+    if (isNaN(newSize) || newSize < 1 || newSize > 100) {
+        alert('Please enter a valid size! (from 1 to 100)');
+        return;
+    }
+
+    // remove current canvas;
+    let canvasContainer = document.querySelector('#canvas-container');
+    let canvas = document.querySelector('.canvas');
+    canvasContainer.removeChild(canvas);
+
+    createCanvas(newSize);
+}
+
+createCanvas(16);
+let resetBtn = document.querySelector('#reset-canvas');
+resetBtn.addEventListener('click', resetCanvas);
+
+let resizeBtn = document.querySelector('#resize-canvas');
+resizeBtn.addEventListener('click', resizeCanvas);
+
+let colorScheme = document.querySelector('#color-scheme');
+colorScheme.addEventListener('change', (e) => curColor = e.target.value);
